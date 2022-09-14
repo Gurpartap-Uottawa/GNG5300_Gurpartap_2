09c01f2319066813cfb0bb1928039703109d5263
dhillonajender <97201745+dhillonajender@users.noreply.github.com>
2022-09-14 01:24:42 -0400

    Update classExercise_13sept2022.py

--- classExercise_13sept2022.py
+++ classExercise_13sept2022.py
@@ -1,9 +1,21 @@
 first_number = input("Enter first number:")
 second_number = input("Enter second number:")
+third_number = input("Enter third number:")
 first_number = int(first_number)
 second_number = int(second_number)
-print("Addition : " + str(first_number + second_number))
-print("Subtraction : " + str(first_number - second_number))
-print("Division : " + str(first_number / second_number))
-print("Multiplication : " + str(first_number * second_number))
-print("Exponential : " + str(first_number ** second_number))
+third_number = int(third_number)
+sum= first_number + second_number
+sub =first_number - second_number
+div = first_number / second_number
+mul = first_number * second_number
+power = first_number ** second_number
+sum_final= sum + third_number
+sub_final =sub - third_number
+div_final = div / third_number
+mul_final = mul * third_number
+power_final = power ** third_number
+print("Addition : " + str(sum_final))
+print("Subtraction : " + str(sub_final))
+print("Division : " + str(div_final))
+print("Multiplication : " + str(mul_final))
+print("Exponential : " + str(power_final))
